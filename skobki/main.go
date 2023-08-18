e0d233cfe880c5a0f96392f146cea8745403690e
Artem Stoyakin <astoyakin@ozon.ru>
2023-08-18 19:25:00 +0300

    skobki

---
+++ skobki/main.go
@@ -0,0 +1,34 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(skobki("()((()))()"))
+	fmt.Println(skobki("()(((()))()"))
+}
+
+// ()((()))() - ok
+// ()(((()))() - не ок
+
+func skobki(skb string) bool {
+	stek := make([]byte, 0, len(skb))
+
+	for i := 0; i < len(skb); i++ {
+		char := skb[i]
+		if char == '(' {
+			stek = append(stek, char)
+		} else {
+			// pop
+			if len(stek) == 0 {
+				return false
+			}
+			stek = stek[1:]
+		}
+	}
+
+	if len(stek) > 0 {
+		return false
+	}
+
+	return true
+}
